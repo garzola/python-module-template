e4b0525c031e518c4357190277942d3067d070d6
Gustavo Arzola <gustavo@xcode.com>
2023-05-21 22:01:09 -0700

    inital commit

---
+++ tests/test_1.py
@@ -0,0 +1,12 @@
+# ==============================================================================================
+# Copyright © 2023 Gustavo Arzola. ALL RIGHTS RESERVED
+# ==============================================================================================
+
+import pytest
+
+class TestOne:
+    def test_one(self) -> None:
+        assert True
+
+    def test_two(self) -> None:
+        assert True
